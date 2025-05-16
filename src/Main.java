65a69b08bf8f585e41d99a46309d458bb81fb332
Amirshain830 <mohamadia87@yahoo.com>
2025-05-17 02:14:14 +0330

    Job list updated

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,90 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+class Job{
+    private String title;
+    private boolean checkout;
+    private String crateat;
+    Job(String title){
+        this.title = title;
+        this.checkout = false;
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        crateat = now.format(formatter);
+    }
+    void setCheckout(){
+        this.checkout = true;
+    }
+    boolean isCheckout(){
+        return this.checkout;
+    }
+    String getCrateat(){
+        return this.crateat;
+    }
+    String getTitle(){
+        return this.title;
+    }
+}
+
+class ToDoList{
+    private ArrayList<Job> job_list = new ArrayList<>();
+
+    void addJob(String title){
+        job_list.add(new Job(title));
+    }
+    void subjob(int index){
+        job_list.remove(index);
+    }
+    void done(int index){
+        job_list.get(index).setCheckout();
+    }
+    void printList(){
+        int i = 0;
+        for (Job lists : job_list) {
+
+            System.out.println( i + " : " + (lists.isCheckout() ? "[✓]" : "[ ]") + " " + lists.getTitle() + " " + lists.getCrateat() );
+            i+=1;
+        }
+    }
+
+}
+
+
 public class Main {
     public static void main(String[] args) {
-
         Scanner cin = new Scanner(System.in);
-        System.out.print("Enter number 1 : ");
-        int num1 = cin.nextInt();
-        System.out.print("Enter number 2 : ");
-        int num2 = cin.nextInt();
-        cin.nextLine();
-        System.out.print("Enter operation : ");
-        char op = cin.nextLine().charAt(0);
-        double final_number=0;
-        switch (op) {
-            case '+':
-                final_number = num1 + num2;
-                break;
-            case '-':
-                final_number = num1 - num2;
-                break;
-            case '*':
-                final_number = num1 * num2;
-                break;
-            case '/':
-                final_number = (double) num1 / num2;
-        }
+        ToDoList list = new ToDoList();
+        while (true) {
+            for (int i=0; i<5; i++) {
+                System.out.println();
+            }
+            System.out.println(" <<<<<<   Hello To Do List :)   >>>>>> \n There are List of jobs of you to do it : ");
+            list.printList();
+            System.out.println("Enter Your Choice: 1=>add , 2=>sub , 3=>done ,4=>exit");
+            int choice = Integer.parseInt(cin.nextLine());
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter Job name: ");
+                    list.addJob(cin.nextLine());
+                    break;
+                case 2:
+                    System.out.println("Enter Job index: ");
+                    list.subjob(Integer.parseInt(cin.nextLine()));
+                    break;
+                case 3:
+                    System.out.println("Enter Job index: ");
+                    list.done(Integer.parseInt(cin.nextLine()));
+                    break;
+                case 4:
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+            }
 
-        System.out.printf("Final number is : %.3f" , final_number );
+
+        }
     }
 }
